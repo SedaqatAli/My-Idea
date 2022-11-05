0cf1ed8063b2c212e401415ee0d8592459ec519d
Ali <sedaqatali9@gmail.com>
2022-11-05 04:34:44 -0700

    added my idea project

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// function mymap(){
+//     let mapCanvas =document.getElementsByClassName('map');
+//         let mapOption={
+//             center:newgoogle.maps.LatLng(51.5,-02),
+//             zoom:10
+//     };
+//     let map = newgoogle.maps.map(mapCanvas,mapOption);
+// }
